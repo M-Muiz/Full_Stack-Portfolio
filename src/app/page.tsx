2be7cc58cdfa725dbdbd70ea23e0193d6f8251c4
m-muiz <mmuizsiddiqui@gmail.com>
2024-06-08 00:14:30 +0500

    major changes

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <main>homeape
+
+<Button>Hello World</Button>
+
+ </main>
+  );
+}
