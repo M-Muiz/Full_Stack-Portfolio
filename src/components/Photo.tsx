757fbcfd797a73cade8b35adb2e4948403b8ee5f
m-muiz <mmuizsiddiqui@gmail.com>
2024-06-12 00:46:14 +0500

    major chnages

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 
 const Photo = () => {
-    
+
     const { theme } = useTheme()
     return (
         <div className="w-full h-full relative">
@@ -19,16 +19,16 @@ const Photo = () => {
 
             <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]" fill="transparent" viewBox="0 0 506 506" xmlns="http://www.w3.org/2000/svg">
                 <motion.circle cx="253" cy="253" r="250" stroke={theme === "dark" ? "#7c0f75" : "#930000"} strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"
-                initial={{strokeDasharray: "24 10 0 0"}} 
-                animate={{
-                    strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-                    rotate: [120, 360]
-                }}
-                transition={{
-                    duration: 20,
-                    repeat: Infinity,
-                    repeatType: "reverse"
-                }}
+                    initial={{ strokeDasharray: "24 10 0 0" }}
+                    animate={{
+                        strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+                        rotate: [120, 360]
+                    }}
+                    transition={{
+                        duration: 20,
+                        repeat: Infinity,
+                        repeatType: "reverse"
+                    }}
                 />
             </motion.svg>
         </div>
