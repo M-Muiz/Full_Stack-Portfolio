3dfe625801931e8f88b26e16da0d5f86859a039a
m-muiz <mmuizsiddiqui@gmail.com>
2024-06-19 21:50:18 +0500

    working on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,13 +5,15 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { MdOutlineConstruction } from "react-icons/md";
 
 
 
 const page = () => {
   return (
-    <div>
-      contact
+    <div className="flex gap-6 mt-4 text-4xl xl:text-8xl justify-center">
+      <MdOutlineConstruction />
+      Working
     </div>
   )
 }
