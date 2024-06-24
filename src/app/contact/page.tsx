48b2dbdd612003413b90196b0c85a7b7c862be3f
m-muiz <mmuizsiddiqui@gmail.com>
2024-06-24 20:48:36 +0500

    complete send message functionality

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,12 +4,11 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { MdOutlineConstruction } from "react-icons/md";
 import { FaEnvelope, FaPhoneAlt } from "react-icons/fa";
 import { FaEarthAsia } from "react-icons/fa6";
 import { motion } from "framer-motion";
-
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 
 const info = [
   {
@@ -29,22 +28,51 @@ const info = [
   },
 ];
 
-
 const page = () => {
+
+  const form = useRef();
+  const [loading, setLoading] = useState(false);
+
+
+  const sendEmail = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      const res = await emailjs.sendForm(
+        "service_bw0axwa",
+        "template_e1s01s1",
+        form.current as any,
+        "7rZJQACfHvmnUQEgJ"
+
+      )
+      setLoading(false);
+      console.log('Message Send Successfully!', res);
+
+    } catch (error) {
+      setLoading(false);
+      console.log('Message Send Failed. Try Again!');
+    }
+  };
+
+
   return (
     <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }} className="py-6">
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 secondaryBackground rounded-xl">
+            <form ref={form as any} onSubmit={sendEmail} className="flex flex-col gap-6 p-10 secondaryBackground rounded-xl">
               <h3 className="text-2xl xl:text-4xl text-primary">Let's Work Together</h3>
               <p className="secondaryText text-sm">I am excited to contribute to a professional team and work on meaningful projects as a FullStack Web Developer.</p>
               <div className="grid grid-cols-1 gap-6">
-                <Input type="text" placeholder="Your Name" className="max-w-[350px]" />
-                <Input type="email" placeholder="Email Address" className="max-w-[350px]" />
+                <Input name="from_name" type="text" placeholder="Your Name" className="max-w-[350px]" />
+                <Input name="from_email" type="email" placeholder="Email Address" className="max-w-[350px]" />
               </div>
-              <Textarea className="h-[200px]" placeholder="Type your message here" />
-              <Button size="md" className="max-w-40">Send Message</Button>
+              <Textarea name="message" className="h-[200px]" placeholder="Type your message here" />
+              <Button
+                value="Send" type="submit" size="md" className="max-w-40 disabled:opacity-60" disabled={loading}>
+                {loading ? "Sending..." : "Send"}
+              </Button>
             </form>
           </div>
 
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] rounded-md border border-black/80 dark:border-slate-400/70 dark:focus:border-primary focus:border-primary bg-transparent tracking-wide px-4 py-5 w-full text-base placeholder:text-primary outline-none resize-none transition-all duration-500",
+          "flex min-h-[80px] rounded-md border border-black/80 dark:border-slate-400/70 dark:focus:border-primary focus:border-primary bg-transparent tracking-wide px-4 py-5 w-full text-sm placeholder:text-primary outline-none resize-none transition-all duration-500 dark:focus:placeholder:text-white focus:placeholder:text-black",
           className
         )}
         ref={ref}
