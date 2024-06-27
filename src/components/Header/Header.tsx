c30b1ad119c82faef283f9256282a25784961de0
m-muiz <mmuizsiddiqui@gmail.com>
2024-06-28 00:25:49 +0500

    fixing error and minor changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,17 @@
+"use client"
+
 import React from 'react'
 import Link from 'next/link'
 import { Button } from '../ui/button'
 import Nav from './Nav'
 import MobileNav from './MobileNav'
 import { ToggleTheme } from '../ToggleTheme'
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
+
+  const router = useRouter();
+
   return (
     <header className='py-8 xl:py-12'>
       <div className="container mx-auto flex items-center justify-between">
@@ -19,7 +25,9 @@ const Header = () => {
         <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <div className="flex items-center gap-2">
-            <Button>Hire Me</Button>
+            <Button onClick={() => router.push('/contact')}>
+              Hire Me
+            </Button>
             <ToggleTheme />
           </div>
 
